b2735ea95be51d32abb5e970491734a9ff90c96f
odessabear@gmail.com <odessabear@gmail.com>
2017-08-31 13:07:19 +0300

    Initial commit

---
+++ src/main/java/com/amikhailov/App.java
@@ -0,0 +1,17 @@
+package com.amikhailov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void printFromLibrary() {
+        System.out.println("I'm from a library!");
+    }
+}
